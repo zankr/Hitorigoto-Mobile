c432ed50381fc3974226419219dc118ea41a536f
zankr <fauzankurniarahman@gmail.com>
2023-11-30 00:36:41 +0700

    getting started

---
+++ app/src/main/java/com/example/hitorigoto/GettingStarted.java
@@ -0,0 +1,14 @@
+package com.example.hitorigoto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GettingStarted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getting_started);
+    }
+}
