8c81487c9c9fd1865974a168aab4aceb7dbcfd60
Daffa Nurrizqi <ndaffaraihan@gmail.com>
2023-12-02 04:07:55 +0700

    Added model for User

---
+++ app/src/main/java/com/example/hitorigoto/Models/User.java
@@ -0,0 +1,41 @@
+package com.example.hitorigoto.Models;
+
+public class User {
+    public int id_user;
+    public String full_name;
+    public String email;
+    public String password;
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
