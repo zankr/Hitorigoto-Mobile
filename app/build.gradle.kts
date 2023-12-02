cc48b8cbb03e17ad3ee430f1190f565f81a67774
zankr <fauzankurniarahman@gmail.com>
2023-12-02 20:19:03 +0700

    added fragment about

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.codesgood:justifiedtextview:1.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
